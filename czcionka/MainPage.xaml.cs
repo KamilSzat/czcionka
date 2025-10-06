b3ad371e52e4d5c06ae808a6d4e65854073cd434
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-10-06 09:50:06 +0200

    czcionki

---
+++ czcionka/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace czcionka
+{
+    public partial class MainPage : ContentPage
+    {
+        private int obecnie = 0;
+        private readonly List<string> greetings = new List<string>
+            {
+                "Dzień dobry",
+                "Good morning",
+                "Buenos dias",
+                "Alloha"
+            };
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            label1.Text = greetings[obecnie]; 
+        }
+
+      
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+            obecnie = (obecnie + 1) % greetings.Count;
+            label1.Text = greetings[obecnie];
+        }
+    }
+
+}
